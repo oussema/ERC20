04faec89993c88dfd14e18b8bb58c2eaa03ea665
oussema GHARSALLAOUI <ossema.gharsallaoui@gmail.com>
2020-04-25 11:43:01 +0100

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -3,8 +3,8 @@ const {abi,bytecode}=require('./compile');
 const HDWalletProvider = require('truffle-hdwallet-provider');
 
 const provider = new HDWalletProvider(
-    '947d0458f84e82e67f85a7cd725ccfacdcb57e11a868864db2ab41556391f5c1',
-    'https://rinkeby.infura.io/v3/14ea7e91103e495c981ce73e342108db'
+    ' ',   // use your private key 
+    ' '       // use your infura api
   );
   const web3 = new Web3(provider);
 
@@ -14,7 +14,7 @@ const deploy = async () => {
     console.log('Attempting to deploy from account', accounts[0]);
   
     const result = await new web3.eth.Contract(abi)
-      .deploy({ data: '0x'+bytecode,arguments:["khawla","KHW",1,100000] })
+      .deploy({ data: '0x'+bytecode,arguments:["TokenName","Symbol",1,100000] }) //Edit arguments 
       .send({  from: accounts[0] });
     
     console.log('Contract deployed to', result.options.address);
